5949b3742448a5abae09bd1d169e9fc216ab3481
MarieDechaux <marie.dechaux@gmail.com>
2021-02-03 10:19:35 +0100

    add react app

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Library</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
