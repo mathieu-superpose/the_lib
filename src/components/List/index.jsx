01303f7f17e3dd8c47f252812da29833126920c0
Mathieu JOLY <m.joly@live.fr>
2021-02-03 18:03:44 +0100

    add search

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Book from 'components/Book';
+import Search from 'components/Search';
 
 const List = () => {
   const [list, setList] = React.useState('');
+  const [search, setSearch] = React.useState(null);
 
   const fetchList = () => {
     fetch('https://gist.githubusercontent.com/MathisDYKDan/76bc73ec77481ccb82677cc7c0d8b524/raw/a23c99027b9bfc1bfdb22e22ddcb4301a5f870ee/books.json')
@@ -12,18 +14,31 @@ const List = () => {
       });
   };
 
-  console.log(list);
-
   React.useEffect(() => {
     fetchList()
   }, []);
 
+  const handleChange = (value) => {
+   setSearch(value);
+ };
+
+ console.log(search);
+ if(list !== "") console.log(list[0].title.includes("Android"));
+
+
   return (
-    <div className="display-cards">
-      {list && (list.map((book) => (
-        <Book data={book} key={book.id} />
-        ))
-      )};
+    <div>
+      <Search onInputValueChanged={handleChange}/>
+      <div className="display-cards">
+        {list && !search && (list.map((book) => (
+          <Book data={book} key={book.id} />
+          ))
+        )}
+        {list && search && (list.filter((book) => book.title.toLowerCase().includes(search) === true).map((book) => (
+          <Book data={book} key={book.id} />
+          ))
+        )}
+      </div>
     </div>
   );
 };
