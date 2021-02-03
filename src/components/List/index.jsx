23e725994157f99710b6a2fb73899936bc7c8d02
Mathieu JOLY <m.joly@live.fr>
2021-02-03 18:44:33 +0100

    add aforite

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -5,6 +5,7 @@ import Search from 'components/Search';
 const List = () => {
   const [list, setList] = React.useState('');
   const [search, setSearch] = React.useState(null);
+  const [favList, setFavList] = React.useState([]);
 
   const fetchList = () => {
     fetch('https://gist.githubusercontent.com/MathisDYKDan/76bc73ec77481ccb82677cc7c0d8b524/raw/a23c99027b9bfc1bfdb22e22ddcb4301a5f870ee/books.json')
@@ -22,20 +23,28 @@ const List = () => {
    setSearch(value);
  };
 
- console.log(search);
- if(list !== "") console.log(list[0].title.includes("Android"));
-
+ const handleFav = (isbn) => {
+    setFavList([...favList, isbn]);
+  };
 
   return (
     <div>
       <Search onInputValueChanged={handleChange}/>
       <div className="display-cards">
         {list && !search && (list.map((book) => (
-          <Book data={book} key={book.id} />
+          <Book 
+            data={book}
+            onFav={ () => {handleFav(book.isbn)}}
+            isFav={favList.includes(book.isbn)}
+            key={`${book.isbn}` } />
           ))
         )}
         {list && search && (list.filter((book) => book.title.toLowerCase().includes(search) === true).map((book) => (
-          <Book data={book} key={book.id} />
+          <Book 
+            data={book}
+            onFav={ () => {handleFav(book.isbn)}}
+            isFav={favList.includes(book.isbn)}
+            key={`${book.isbn}` } />
           ))
         )}
       </div>
